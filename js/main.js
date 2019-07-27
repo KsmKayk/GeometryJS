61e8cd0f00c19a20483656ffdecfa3a7be19f471
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2019-07-27 01:56:51 -0300

    Adicionando o requirejs para carregar os arquivos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+(function(undefined) {
+  "use strict";
+
+  require(["chao", "personagem", "jogo"], function() {
+    const jogo = new Jogo();
+    jogo.configurar();
+
+    const loop = function() {
+      jogo.rodar();
+      window.requestAnimationFrame(loop);
+    };
+    window.requestAnimationFrame(loop);
+  });
+})();
+
 //let altura = window.innerHeight;
 //let largura = window.innerWidth;
-const jogo = new Jogo();
-jogo.configurar();
-
-const loop = function() {
-  jogo.rodar();
-  window.requestAnimationFrame(loop);
-};
-window.requestAnimationFrame(loop);
 
 //window.requestAnimationFrame(this.rodar);
 //document.addEventListener("mousedown", clique);
